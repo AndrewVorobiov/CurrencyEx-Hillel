9b404b335525279109f236e03a1621ee35716b2b
AndrewVorobiov <dazhbog0@gmail.com>
2021-09-07 18:31:37 +0300

    HW4

---
+++ app/currency/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+#from currency.utils import generate_password as gp
+
+
+def generate_password(request):
+    #password = gp()
+    return HttpResponse('Hello World!')
+
