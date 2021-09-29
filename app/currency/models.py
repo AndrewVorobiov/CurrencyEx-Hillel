dc47eb655c5689e7c89a4c13ab192097447e591f
AndrewVorobiov <dazhbog0@gmail.com>
2021-09-29 12:22:12 +0300

    adding contact us

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    type = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=64)
+class ContactUs(models.Model):
+    id = models.AutoField(primary_key=True)
+    email_from = models.EmailField(max_length=30)
+    subject = models.CharField(max_length=25)
+    message = models.TextField()
+
+
